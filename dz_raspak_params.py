e5a035cabc7c35919814801cab3aa4c418517a54
ArDa0226 <161866557+ArDa0226@users.noreply.github.com>
2024-03-06 21:34:08 +0500

    Домашнее задание по распаковке параметров

---
+++ dz_raspak_params.py
@@ -0,0 +1,18 @@
+#Функция с параметрами
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+print_params()
+
+print_params(b=25)
+print_params(c=[1,2,3])
+
+#Распаковка параментров
+values_list = [1, 'a', {'art':123}]
+values_dict = {'a': 123, 'b':[345], 'c': 'scare'}
+
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [[1,2,3],('qwe')]
+print_params(*values_list_2, 42)
